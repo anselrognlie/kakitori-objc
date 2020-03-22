2caa22ec6c2aca384578d90b8e5544734acea40b
Ansel Rognlie <ansel.rognlie@gmail.com>
2020-03-22 13:10:18 -0700

    Initial Commit

---
+++ xml-workspace/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  xml-workspace
+//
+//  Created by Ansel Rognlie on 3/22/20.
+//  Copyright © 2020 Ansel Rognlie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
